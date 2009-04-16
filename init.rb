e7438464c709f299acc2274aa7f468337384eaff
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2009-04-16 21:32:44 +0000

    Don't reload lib folder since this causes issues with the naming_schemer plugin.

--- init.rb
+++ init.rb
@@ -1,4 +1,3 @@
 # Include hook code here
 ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/app/models')
 ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/app/controllers')
-ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/lib')
