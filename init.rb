f7fe5544d3b014f90c2de4e42ac33ab9bd8f598e
bmarzolf <bmarzolf@53153c1e-2216-0410-80fa-f1be67669deb>
2009-04-16 21:28:46 +0000

    Turn off caching in development mode. This solved an issues with associations between plugin models and main app models, as well as making it so that in development, the server doesn't need a restart every time a change is made.

--- init.rb
+++ init.rb
@@ -1,1 +1,4 @@
 # Include hook code here
+ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/app/models')
+ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/app/controllers')
+ActiveSupport::Dependencies.load_once_paths.delete(File.expand_path(File.dirname(__FILE__))+'/lib')
